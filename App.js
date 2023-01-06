63d9b8c76612250c9e13c9840e105b7a298c7cd5
arthurchina92 <arthurchinaa@gmail.com>
2023-01-05 19:30:52 -0600

    Se creo mainpage y se dio estilo inicial

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import MainPage from './Views/mainpage/mainpage';
+
+const App = () => {
+  return (
+    <MainPage/>
+  );
+};
+
+
+export default App;
---
+++ Views/mainpage/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  body: {
+    backgroundColor: '#00ff00',
+  },
+  imageCard: {
+    width: 400,
+    height: 250,
+    backgroundColor: '#fff',
+    borderWidth: 5,
+    borderColor: '#000',
+    margin: 5,
+  },
+});
